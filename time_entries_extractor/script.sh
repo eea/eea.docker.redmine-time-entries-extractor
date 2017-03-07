14e9af67a46144e5afaba2ed1e2f6e7856db7ee4
Mauro Michielon <mauro.michielon@eea.europa.eu>
2017-03-07 15:40:47 +0100

    Update script.sh

--- time_entries_extractor/script.sh
+++ time_entries_extractor/script.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-/opt/redmine_time_entries_extractor/redmine_time_entries_extractor_run.sh --context_param redmineUrl=REDMINEURL --context_param userName=USERNAME --context_param password=PASSWORD --context_param timeFrom=TIMEFROM --context_param timeTo=TIMETO --context_param outputDir=OUTPUTDIR --context_param dbDatabase=DBDATABASE --context_param dbHost=DBHOST --context_param dbPort=DBPORT --context_param dbUsername=DBUSERNAME --context_param dbPassword=DBPASSWORD --context_param dbSchema=DBSCHEMA
+/opt/redmine_time_entries_extractor/redmine_time_entries_extractor_run.sh --context_param redmineUrl=REDMINEURL --context_param userName=USERNAME --context_param password=PASSWORD --context_param timeFrom=TIMEFROM --context_param timeTo=TIMETO --context_param outputDir=OUTPUTDIR --context_param dbDatabase=DBDATABASE --context_param dbHost=DBHOST --context_param dbPort=DBPORT --context_param dbUsername=DBUSERNAME --context_param dbPassword=DBPASSWORD --context_param dbSchema=DBSCHEMA --context_param forceDelete=FORCEDELETE
