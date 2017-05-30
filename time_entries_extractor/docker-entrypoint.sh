a24fdc3e9f05d8229df9eabc6d9403e7d2f8a931
Mauro Michielon - EEA 06Oct2015 <michimau@kvm-cph-06.eea.eu.int>
2017-05-30 15:01:52 +0200

    better backup strategy

--- time_entries_extractor/docker-entrypoint.sh
+++ time_entries_extractor/docker-entrypoint.sh
@@ -2,64 +2,68 @@
 
 set -e
 
-sed "s#REDMINEURL#$REDMINEURL#g" -i /opt/script.sh
-sed "s#TIMEFROM#$TIMEFROM#g" -i /opt/script.sh
-sed "s#TIMETO#$TIMETO#g" -i /opt/script.sh
-sed "s#REDMINEUSERNAME#$REDMINEUSERNAME#g" -i /opt/script.sh
-sed "s#REDMINEPASSWORD#$REDMINEPASSWORD#g" -i /opt/script.sh
-sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /opt/script.sh
+counter=0
+while [ ! "$(curl http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200 2> /dev/null)" -a $counter -lt 100  ]; do
+  sleep 1
+  let counter=counter+1
+  echo "waiting for Elasticsearch to be up ($counter/100)"
+done
 
-sed "s#DBHOST#$DBHOST#g" -i /opt/script.sh
-sed "s#DBDATABASE#$DBDATABASE#g" -i /opt/script.sh
-sed "s#DBSCHEMA#$DBSCHEMA#g" -i /opt/script.sh
-sed "s#DBPORT#$DBPORT#g" -i /opt/script.sh
-sed "s#APIKEY#$APIKEY#g" -i /opt/script.sh
+#sed "s#REDMINEURL#$REDMINEURL#g" -i /opt/script.sh
+#sed "s#TIMEFROM#$TIMEFROM#g" -i /opt/script.sh
+#sed "s#TIMETO#$TIMETO#g" -i /opt/script.sh
+#sed "s#REDMINEUSERNAME#$REDMINEUSERNAME#g" -i /opt/script.sh
+#sed "s#REDMINEPASSWORD#$REDMINEPASSWORD#g" -i /opt/script.sh
+#sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /opt/script.sh
 
-sed "s#DBUSERNAME#$DBUSERNAME#g" -i /opt/script.sh
-sed "s#DBPASSWORD#$DBPASSWORD#g" -i /opt/script.sh
+#sed "s#DBHOST#$DBHOST#g" -i /opt/script.sh
+#sed "s#DBDATABASE#$DBDATABASE#g" -i /opt/script.sh
+#sed "s#DBSCHEMA#$DBSCHEMA#g" -i /opt/script.sh
+#sed "s#DBPORT#$DBPORT#g" -i /opt/script.sh
+#sed "s#APIKEY#$APIKEY#g" -i /opt/script.sh
 
-sed "s#FORCEDELETE#$FORCEDELETE#g" -i /opt/script.sh
+#sed "s#DBUSERNAME#$DBUSERNAME#g" -i /opt/script.sh
+#sed "s#DBPASSWORD#$DBPASSWORD#g" -i /opt/script.sh
 
-sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/script.sh
+#sed "s#FORCEDELETE#$FORCEDELETE#g" -i /opt/script.sh
+
+#sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/script.sh
 sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/script.sh
-sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/ingestKibana.sh
-sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/ingestKibana.sh
+#sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/ingestKibana.sh
+#sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/ingestKibana.sh
 
 sed "s#INDEXNAME#$INDEXNAME#g" -i /time_entries.conf
 
-if [ "$RESETATSTARTUP" = "YES" ] && [ -d "$KIBANACONFIGURATIONDIR/configurationKibana" ];
+if [ "$RESETATSTARTUP" = "YES" ] || [ ! -d "/eea.kibana.configs" ];
 then
-    cd $KIBANACONFIGURATIONDIR/configurationKibana 
-    git pull
-#    rm -rf $KIBANACONFIGURATIONDIR
+  git clone https://github.com/eea/eea.kibana.configs.git /eea.kibana.configs
+  if [ -d "/eea.kibana.configs/$INDEXNAME" ];
+    then
+      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_mapping.json --output=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_analyzer.json --output=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_data.json --output=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+  fi
+else
+  cd /eea.kibana.configs
+  git pull
 fi
-
-if [ ! -d "$KIBANACONFIGURATIONDIR/configurationKibana" ]
-then
-  git clone https://github.com/eea/eea.kibana.configs.git $KIBANACONFIGURATIONDIR/configurationKibana
-  cd $KIBANACONFIGURATIONDIR/configurationKibana/$INDEXNAME
-#  git checkout $INDEXNAME
-fi
-
 #ls $KIBANACONFIGURATIONDIR/$INDEXNAME
 
-if [ ! -z "$LOGSTASH_RW_USERNAME" ]; then
+#if [ ! -z "$LOGSTASH_RW_USERNAME" ]; then
     sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /time_entries.conf
     sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /time_entries.conf
     sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /time_entries.conf
     mkdir -p /usr/share/logstash/config
     cp /time_entries.conf /usr/share/logstash/config/time_entries.conf
 
-    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /opt/script.sh
-    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /opt/script.sh
+#    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /opt/script.sh
+#    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /opt/script.sh
 
-    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /opt/ingestKibana.sh
-    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /opt/ingestKibana.sh
-fi
+#    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /opt/ingestKibana.sh
+#    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /opt/ingestKibana.sh
+#fi
 
-sleep 5
-
-sh /opt/ingestKibana.sh
+#sh /opt/ingestKibana.sh
 sh /opt/script.sh
 
 exec "$@" 
--- time_entries_extractor/script.sh
+++ time_entries_extractor/script.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-elasticdump --input=http://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=mapping  > KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_mapping.json 
-elasticdump --input=http://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/kibana --output=$ --type=analyzer  > KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_analyzer.json
-elasticdump --input=http://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=data  > KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_data.json
+/opt/redmine_time_entries_extractor/redmine_time_entries_extractor_run.sh --context_param redmineUrl=$REDMINEURL --context_param apikey=$APIKEY --context_param outputDir=$OUTPUTDIR --context_param dbDatabase=$DBDATABASE --context_param dbHost=$DBHOST --context_param dbPort=$DBPORT --context_param dbUsername=$DBUSERNAME --context_param dbPassword=$DBPASSWORD --context_param dbSchema=$DBSCHEMA --context_param timeFrom=$TIMEFROM --context_param timeTo=$TIMETO --context_param forceDelete=$FORCEDELETE
 
-/opt/redmine_time_entries_extractor/redmine_time_entries_extractor_run.sh --context_param redmineUrl=REDMINEURL --context_param apikey=APIKEY --context_param outputDir=OUTPUTDIR --context_param dbDatabase=DBDATABASE --context_param dbHost=DBHOST --context_param dbPort=DBPORT --context_param dbUsername=DBUSERNAME --context_param dbPassword=DBPASSWORD --context_param dbSchema=DBSCHEMA --context_param timeFrom=TIMEFROM --context_param timeTo=TIMETO --context_param forceDelete=FORCEDELETE
+export TESTKIBANAINDEX=$(curl --user $LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD -s 'http://elasticsearch:9200/_cat/indices?' | grep .kibana)
+echo "testing in script.sh"
+if [ ! -z "$TESTKIBANAINDEX" ]
+then
+  mkdir -p /$KIBANACONFIGURATIONDIR/INDEXNAME
+  elasticdump --input=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=mapping   > /$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_mapping.json
+  elasticdump --input=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=analyzer  > /$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_analyzer.json
+  elasticdump --input=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana --output=$ --type=data      > /$KIBANACONFIGURATIONDIR/INDEXNAME/kibana_data.json
+fi
+export TESTKIBANAINDEX=''
+
+
