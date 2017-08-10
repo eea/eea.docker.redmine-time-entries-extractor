371408df8eb73163b1b6747496540a48cd28c545
Mauro Michielon <mauro.michielon@eea.europa.eu>
2017-08-10 17:37:03 +0200

    https fix

--- time_entries_extractor/docker-entrypoint.sh
+++ time_entries_extractor/docker-entrypoint.sh
@@ -3,34 +3,15 @@
 set -e
 
 counter=0
-while [ ! "$(curl http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200 2> /dev/null)" -a $counter -lt 100  ]; do
+while [ ! "$(curl -k https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200 2> /dev/null)" -a $counter -lt 100  ]; do
   sleep 1
   let counter=counter+1
   echo "waiting for Elasticsearch to be up ($counter/100)"
 done
 
-#sed "s#REDMINEURL#$REDMINEURL#g" -i /opt/script.sh
-#sed "s#TIMEFROM#$TIMEFROM#g" -i /opt/script.sh
-#sed "s#TIMETO#$TIMETO#g" -i /opt/script.sh
-#sed "s#REDMINEUSERNAME#$REDMINEUSERNAME#g" -i /opt/script.sh
-#sed "s#REDMINEPASSWORD#$REDMINEPASSWORD#g" -i /opt/script.sh
-#sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /opt/script.sh
+NODE_TLS_REJECT_UNAUTHORIZED=0
 
-#sed "s#DBHOST#$DBHOST#g" -i /opt/script.sh
-#sed "s#DBDATABASE#$DBDATABASE#g" -i /opt/script.sh
-#sed "s#DBSCHEMA#$DBSCHEMA#g" -i /opt/script.sh
-#sed "s#DBPORT#$DBPORT#g" -i /opt/script.sh
-#sed "s#APIKEY#$APIKEY#g" -i /opt/script.sh
-
-#sed "s#DBUSERNAME#$DBUSERNAME#g" -i /opt/script.sh
-#sed "s#DBPASSWORD#$DBPASSWORD#g" -i /opt/script.sh
-
-#sed "s#FORCEDELETE#$FORCEDELETE#g" -i /opt/script.sh
-
-#sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/script.sh
 sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/script.sh
-#sed "s#KIBANACONFIGURATIONDIR#$KIBANACONFIGURATIONDIR#g" -i /opt/ingestKibana.sh
-#sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/ingestKibana.sh
 
 sed "s#INDEXNAME#$INDEXNAME#g" -i /time_entries.conf
 
@@ -39,9 +20,9 @@ then
   git clone https://github.com/eea/eea.kibana.configs.git /eea.kibana.configs
   if [ -d "/eea.kibana.configs/$INDEXNAME" ];
     then
-      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_mapping.json --output=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
-      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_analyzer.json --output=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
-      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_data.json --output=http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_mapping.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_analyzer.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      elasticdump --input=/eea.kibana.configs/$INDEXNAME/kibana_data.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
   fi
 else
   cd /eea.kibana.configs
@@ -49,21 +30,12 @@ else
 fi
 #ls $KIBANACONFIGURATIONDIR/$INDEXNAME
 
-#if [ ! -z "$LOGSTASH_RW_USERNAME" ]; then
-    sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /time_entries.conf
-    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /time_entries.conf
-    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /time_entries.conf
-    mkdir -p /usr/share/logstash/config
-    cp /time_entries.conf /usr/share/logstash/config/time_entries.conf
+sed "s#OUTPUTDIR#$OUTPUTDIR#g" -i /time_entries.conf
+sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /time_entries.conf
+sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /time_entries.conf
+mkdir -p /usr/share/logstash/config
+cp /time_entries.conf /usr/share/logstash/config/time_entries.conf
 
-#    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /opt/script.sh
-#    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /opt/script.sh
-
-#    sed "s#LOGSTASH_RW_USERNAME#$LOGSTASH_RW_USERNAME#g" -i /opt/ingestKibana.sh
-#    sed "s#LOGSTASH_RW_PASSWORD#$LOGSTASH_RW_PASSWORD#g" -i /opt/ingestKibana.sh
-#fi
-
-#sh /opt/ingestKibana.sh
 sh /opt/script.sh
 
 exec "$@" 
--- time_entries_extractor/ingestKibana.sh
+++ time_entries_extractor/ingestKibana.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e
+NODE_TLS_REJECT_UNAUTHORIZED=0
 
-elasticdump --input=KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_mapping.json --output=http://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
-elasticdump --input=KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_analyzer.json --output=http://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/kibana
-elasticdump --input=KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_data.json --output=http://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+elasticdump --input=KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_mapping.json --output=https://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+elasticdump --input=KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_analyzer.json --output=https://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/kibana
+elasticdump --input=KIBANACONFIGURATIONDIR/configurationKibana/INDEXNAME/kibana_data.json --output=https://LOGSTASH_RW_USERNAME:LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
 
-#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_mapping.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
-#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_analyzer.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
-#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME_data.json --output=http://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
+#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_mapping.json --output=https://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
+#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME/INDEXNAME_analyzer.json --output=https://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
+#elasticdump --input=KIBANACONFIGURATIONDIR/INDEXNAME_data.json --output=https://RW_USERNAME:RW_PASSWORD@elasticsearch:9200/INDEXNAME
