a17fe3d34e6aaa1cdc9ce3fb6049ac4e9717ad92
Mauro Michielon <mauro.michielon@eea.europa.eu>
2018-03-08 08:35:26 +0100

    streamline and cleanup

--- time_entries_extractor/docker-entrypoint.sh
+++ time_entries_extractor/docker-entrypoint.sh
@@ -10,12 +10,9 @@ sed "s#INDEXNAME#$INDEXNAME#g" -i /time_entries.conf
 mkdir -p /usr/share/logstash/config
 cp /time_entries.conf /usr/share/logstash/config/time_entries.conf
 
-counter=0
-while [ ! "$(curl -k https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200 2> /dev/null)" -a $counter -lt 100  ]; do
-  sleep 5
-  let counter=counter+1
-  echo "waiting for Elasticsearch to be up ($counter/100)"
-done
+echo "waiting until elasticsearch is up"
+until curl -k https://$RW_USERNAME:$RW_PASSWORD@elasticsearch:9200 &>/dev/null; do sleep 5; done
+echo "elasticsearch is up"
 
 sed "s#INDEXNAME#$INDEXNAME#g" -i /opt/script.sh
 
@@ -24,15 +21,15 @@ then
   git clone https://github.com/eea/eea.kibana.configs.git /eea.kibana.configs
   if [ -d "/eea.kibana.configs/$INDEXNAME" ];
     then
-      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --headers='{"Content-Type": "application/json"}' --input=/eea.kibana.configs/$INDEXNAME/kibana_mapping.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
-      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --headers='{"Content-Type": "application/json"}' --input=/eea.kibana.configs/$INDEXNAME/kibana_analyzer.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
-      NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --headers='{"Content-Type": "application/json"}' --input=/eea.kibana.configs/$INDEXNAME/kibana_data.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200/.kibana
+      cd /eea.kibana.configs/$INDEXNAME
+NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --type=data --output-index=.kibana --headers='{"Content-Type": "application/json"}' --input=kibana_data.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200
+NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --type=mapping --output-index=.kibana --headers='{"Content-Type": "application/json"}' --input=kibana_mapping.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200
+NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --type=analyzer --output-index=.kibana --headers='{"Content-Type": "application/json"}' --input=kibana_analyzer.json --output=https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200
   fi
 else
   cd /eea.kibana.configs
   git pull
 fi
-#ls $KIBANACONFIGURATIONDIR/$INDEXNAME
 
 sh /opt/script.sh
 
