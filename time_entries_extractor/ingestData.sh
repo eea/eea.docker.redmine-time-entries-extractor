cc463bb95bf213de65f453c4402475d0ef6e95d6
Mauro Michielon <mauro.michielon@eea.europa.eu>
2018-04-18 17:56:36 +0200

    change of job's parameters

--- time_entries_extractor/ingestData.sh
+++ time_entries_extractor/ingestData.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/opt/redmine_time_entries_extractor/redmine_time_entries_extractor_run.sh --context_param redmineUrl=$REDMINEURL --context_param apikey=$APIKEY --context_param outputDir=$OUTPUTDIR --context_param dbDatabase=$DBDATABASE --context_param dbHost=$DBHOST --context_param dbPort=$DBPORT --context_param dbUsername=$DBUSERNAME --context_param dbPassword=$DBPASSWORD --context_param dbSchema=$DBSCHEMA --context_param timeFrom=$TIMEFROM --context_param timeTo=$TIMETO --context_param forceDelete=$FORCEDELETE
+/opt/redmine_time_entries_extractor/redmine_time_entries_extractor_run.sh --context_param redmineUrl=$REDMINEURL --context_param redmineApiKey=$APIKEY --context_param outputDir=$OUTPUTDIR --context_param timeFrom=$TIMEFROM --context_param timeTo=$TIMETO --context_param forceDelete=$FORCEDELETE
 
 export TESTKIBANAINDEX=$(curl -k --user $LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD -s 'https://elasticsearch:9200/_cat/indices?' | grep kibana)
 
