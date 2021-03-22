aac2bb8000b408c100843dc2432bc7d3e2478fca
michimau <mauro.michielon@eea.europa.eu>
2021-03-22 13:58:52 +0100

    0.0.6, no ssl on 9200

--- time_entries_extractor/ingestData.sh
+++ time_entries_extractor/ingestData.sh
@@ -4,7 +4,7 @@ set -e
 
 /opt/redmine_time_entries_extractor/redmine_time_entries_extractor_run.sh --context_param ncHost=$NCHOST --context_param ncUsername=$NCUSERNAME --context_param ncPassword=$NCPASSWORD --context_param redmineUrl=$REDMINEURL --context_param redmineApiKey=$APIKEY --context_param outputDir=$OUTPUTDIR --context_param timeFrom=$TIMEFROM --context_param timeTo=$TIMETO --context_param forceDelete=$FORCEDELETE
 
-export TESTKIBANAINDEX=$(curl -k --user $LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD -s 'https://elasticsearch:9200/_cat/indices?' | grep kibana)
+export TESTKIBANAINDEX=$(curl -k --user $LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD -s 'http://elasticsearch:9200/_cat/indices?' | grep kibana)
 
 echo pre import TESTKIBANAINDEX=$TESTKIBANAINDEX
 
@@ -15,11 +15,11 @@ then
   cd /$KIBANACONFIGURATIONDIR/$INDEXNAME 
   echo /$KIBANACONFIGURATIONDIR/$INDEXNAME
 
-NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input-index=.kibana --headers='{"Content-Type": "application/json"}' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200" --output=kibana_mapping.json --type=mapping
-  NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input-index=.kibana --headers='{"Content-Type": "application/json"}' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200" --output=kibana_analyzer.json --type=analyzer
-  NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input-index=.kibana --headers='{"Content-Type": "application/json"}' --input="https://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200" --output=kibana_data.json --type=data
+NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input-index=.kibana --headers='{"Content-Type": "application/json"}' --input="http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200" --output=kibana_mapping.json --type=mapping
+  NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input-index=.kibana --headers='{"Content-Type": "application/json"}' --input="http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200" --output=kibana_analyzer.json --type=analyzer
+  NODE_TLS_REJECT_UNAUTHORIZED=0 elasticdump --input-index=.kibana --headers='{"Content-Type": "application/json"}' --input="http://$LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD@elasticsearch:9200" --output=kibana_data.json --type=data
 
-export TESTKIBANAINDEX=$(curl -k --user $LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD -s 'https://elasticsearch:9200/_cat/indices?' | grep kibana)
+export TESTKIBANAINDEX=$(curl -k --user $LOGSTASH_RW_USERNAME:$LOGSTASH_RW_PASSWORD -s 'http://elasticsearch:9200/_cat/indices?' | grep kibana)
 echo post import TESTKIBANAINDEX=$TESTKIBANAINDEX
 fi
 
